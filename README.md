3526f349a68374d990212560e78c9429346838d6
Siwei.Wang@scotiabank.com <wangsiwei2003@hotmail.com>
2017-03-28 15:06:52 -0400

    add :flags to allow remove template after deployment

--- README.md
+++ README.md
@@ -2,13 +2,41 @@
 
 ## Simple python script to populate the nifi templates and create instance of each Templates on the canvas using nifi Rest API
 
+The propose of this script is to help nifi with CI/CD pipeline to deploy template.
+
+## Support
+
+nifi-api 1.1.1
+
 ### How to use
 
-`python populate.py <NIFI_Server_IP> <NIFI_Server_Port> <NIFI_Template_Dir>`
+1. clone this repo
+
+2. `python populate.py <NIFI_Server_IP> <NIFI_Server_Port> <NIFI_Template_Dir> <REMOVE_AFTER_CREATED>`
+
+NIFI_Server_IP -- the ip address/hostname of nifi server **required**
+
+NIFI_Server_Port -- the port of the nifi server **required**
+
+NIFI_Template_Dir -- the directory of the nifi template **required**
+
+REMOVE_AFTER_CREATED -- boolean value to allow template removal after instances are created **optional, default to false**
+
+for example `python populate.py 192.168.3.10 8080 ./templates true`
+
+this command will import all the template under `./templates` to nifi api located at http://192.168.3.10:8080/nifi-api. It will also remove the templates after instances are created.
 
 ### Python version
 
 2.7
+
+## TODO
+
+Support jinjia templating language for nifi templates.
+
+Support SSL
+
+Better logging message
 
 ### License
 
--- populate.py
+++ populate.py
@@ -3,11 +3,14 @@ import json
 import sys
 import pathlib
 import xml.etree.ElementTree as ET
+import os
+from jinja2 import Template
 
 # Get command args
 hostname = sys.argv[1]
 port = sys.argv[2]
 templateDir = sys.argv[3]
+remove_after_create = sys.argv[4]
 
 print 'Host ip is ' + hostname + ', and port is ' + port
 HostURL= "http://" + hostname + ":" + port + "/nifi-api"
@@ -17,7 +20,6 @@ resource_url = HostURL + "/resources"
 print resource_url
 
 response = requests.get(resource_url)
-print response.json()
 json = response.json()
 
 resources = json["resources"]
@@ -44,23 +46,26 @@ def get_template_name(file):
 def upload_template(template_file_name):
     upload_url = HostURL + "/process-groups/" + resourceId + "/templates/upload"
     print upload_url
+    file_string = open(templateDir+ "/" + template_file_name, 'r').read()
+    template = Template(file_string)
+    rendered= template.render(password=os.environ['es_password'])
     multipart_form_data = {
-      'template': open(templateDir+ "/" + template_file_name, 'rb'),
+      'template': rendered,
     }
     response = requests.post(upload_url, files=multipart_form_data)
     print response
 
+# create an instance using the template id
 def instantiate_template(template_file_name, originX, originY):
-    # create an instance using the template id
     create_instance_url = HostURL + "/process-groups/" + resourceId + "/template-instance"
-    print create_instance_url
-    payload = {"templateId": get_template_id(get_templates(), template_file_name), "originX": originX, "originY": originY}
+    payload = {"templateId": get_template_id(template_file_name), "originX": originX, "originY": originY}
     originX = originX + 600
     originY = originY - 50
     response = requests.post(create_instance_url, json=payload)
     print response
     print "Done."
 
+# get templates
 def get_templates():
     get_template_instance_url = HostURL + "/flow/templates"
     response = requests.get(get_template_instance_url)
@@ -68,25 +73,39 @@ def get_templates():
     templates = json["templates"]
     return templates
 
-# get the template ids
-def get_template_id(templates, template_file_name):
-    templateId = ""
+# get the template id
+def get_template_id(template_file_name):
+    templates = get_templates()
+    template_id = ""
     for template in templates:
       if get_template_name(templateDir+ "/" + template_file_name) == template["template"]["name"]:
-        print "Creating instance of " + template["template"]["name"] + " ..."
-        templateId = template["template"]["id"]
-        return templateId
 
+        print "Creating instance of " + template["template"]["name"] + " ..."
+        template_id = template["template"]["id"]
+    return template_id
+
+def check_and_remove_template(template_id):
+  if template_id != "":
+    delete_template_url = HostURL + "/templates/" + template_id
+    response = requests.delete(delete_template_url)
+    print delete_template_url
+    print response
+    print "deleted template " + template_id
+
+# main function, import templates
 def import_templates():
-  # Random start up position
+  # random start up position
   originX = 661
   originY = -45
 
   for templateFile in pathlib.Path(templateDir).iterdir():
-
     if templateFile.is_file():
-      print "Adding templates " + templateFile.name + " ..."
+      print "importing  templates " + templateFile.name + " ..."
+      check_and_remove_template(get_template_id(templateFile.name))
       upload_template(templateFile.name)
       instantiate_template(templateFile.name, originX, originY)
+      print remove_after_create
+      if remove_after_create == "true":
+        check_and_remove_template(get_template_id(templateFile.name))
 
 import_templates()
